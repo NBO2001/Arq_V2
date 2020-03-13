ae6253113727dae03516eb231c88eb658941e4cd
NBO2001 <54229396+NBO2001@users.noreply.github.com>
2020-03-13 15:32:26 -0400

    Seu funcionar é sucesso

--- etq_uni.php
+++ etq_uni.php
@@ -5,6 +5,76 @@ include_once 'Conec_PDO.php';
 if(isset($_GET['alid'])){
   $if =  $_GET['alid'];
 }
+?>
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <style>
+          *{
+	        font-family:Calibri,Arial Narrow,arial,helvetica;
+          }
+        .folha{
+            page-break-after: always;
+          }
+        #Modelo_body{
+            padding:0px 0px;
+        background-color: inherit;
+        }
+        #Modelo_body table{
+          padding:0px 0px;
+        width:15.5cm;
+        height:25.5cm;
+        border-collapse:collapse;
+        border:1px solid black;
+        overflow: auto;
+        }
+        #Modelo_body .xl72{
+        font-weight: bold;
+        font-size: 45px;
+        height:1.5cm;
+        width:3.5cm;
+        border:1px solid black;
+
+        }
+        #Modelo_body .xl73{
+          width:4.5cm;
+        border:1px solid black;
+        }
+        #Modelo_body td{
+        border-right:.5pt solid black;
+        border-bottom:.5pt solid black;
+        }
+        #Modelo_body .xl74{
+          text-align:left;
+          height:1cm;
+        font-weight: bold;
+        font-size: 30px;
+        }
+        #Modelo_body .xl76{
+          height:1cm;
+        }
+        #Modelo_body .xl78{
+          height:1cm;
+          padding:0px 0px;
+        }
+        #Modelo_body .xl80{
+          font-size: 11px;
+          height:0.5cm;
+        }
+        </style>
+<title>Etiquetas</title>
+</head>
+<body id="Modelo_body">
+
+<div class="div1">
+  <!-- Inicio tabela-->
+<table class="folha">
+  <tbody>
+    
+<?php
 $stmt = $pdo->prepare("SELECT * FROM Alunos WHERE id LIKE $if");
 $stmt->execute(array('id','Cod_cur','Num_mat','Nome_civil','Nome_cur','Fin','Fev','Ain','Aev','sistema'));
 foreach($stmt as $res){
@@ -21,31 +91,164 @@ $sistema = $res['sistema'];
 $tabela = "<tr>
 <td class='xl72' >$curso</td>
 <td class='xl73' >$Nome_cur</td>
+<td class='xl72' ></td>
+<td class='xl73' ></td>
  </tr>
  <tr>
 <td class='xl74' colspan='2' >$Num_mat</td>
+<td class='xl74' colspan='2' ></td>
  </tr>
  <tr>
  <td class='xl76' colspan='2' >$Nome_civil</td>
+  <td class='xl76' colspan='2' ></td>
  </tr>
  <tr>
 <td class='xl78' colspan='2' >$Fin</td>
+<td class='xl78' colspan='2' ></td>
  </tr>
 <tr >
-<td class='xl80' colspan='2' >$Ain</td></tr>
+<td class='xl80' colspan='2' >$Ain</td>
+<td class='xl80' colspan='2' ></td>
+</tr>
 <tr >
-<td class='xl80' colspan='2'>$Fev</td></tr>
+<td class='xl80' colspan='2'>$Fev</td>
+<td class='xl80' colspan='2'></td>
+</tr>
 <tr >
-<td class='xl80' colspan='2'>$Aev</td></tr>
+<td class='xl80' colspan='2'>$Aev</td>
+<td class='xl80' colspan='2'></td>
+</tr>
 <tr>
-<td class='xl80' colspan='2'>$sistema</td></tr>";
-
-
-$tabela ="<table><tbody>".$tabela."</tbody></table>";
-
+<td class='xl80' colspan='2'>$sistema</td>
+<td class='xl80' colspan='2'></td>
+</tr>";
+echo $tabela;
 ?>
+</tbody>
+
+
+   
+
+  <tbody>
+  <tr>
+    <td class="xl72"></td>
+    <td class="xl73"></td>
+    <td class="xl72"></td>
+    <td class="xl73"></td>
+  </tr>
+  <tr>
+    <td class="xl74" colspan="2"></td>
+    <td class="xl74" colspan="2"></td>
+  </tr>
+  <tr>
+    <td class="xl76" colspan="2"></td>
+    <td class="xl76" colspan="2"></td>
+  </tr>
+  <tr>
+  <td class="xl80" colspan="2"></td>
+    <td class="xl80" colspan="2"></td>
+  </tr>
+  <tr>
+    <td class="xl80" colspan="2"></td>
+    <td class="xl80" colspan="2"></td>
+  </tr>
+  <tr>
+    <td class="xl80" colspan="2"></td>
+    <td class="xl80" colspan="2"></td>
+  </tr>
+  <tr>
+    <td class="xl80" colspan="2"></td>
+    <td class="xl80" colspan="2"></td>
+  </tr>
+  <tr>
+    <td class="xl80" colspan="2"></td>
+    <td class="xl80" colspan="2"></td>
+  </tr>
+</tbody>
+
+<tbody>
+<tr>
+  <td class="xl72"></td>
+  <td class="xl73"></td>
+  <td class="xl72"></td>
+  <td class="xl73"></td>
+</tr>
+<tr>
+  <td class="xl74" colspan="2"></td>
+  <td class="xl74" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl76" colspan="2"></td>
+  <td class="xl76" colspan="2"></td>
+</tr>
+<tr>
+    <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+</tbody>
+<tbody>
+<tr>
+  <td class="xl72"></td>
+  <td class="xl73"></td>
+  <td class="xl72"></td>
+  <td class="xl73"></td>
+</tr>
+<tr>
+  <td class="xl74" colspan="2"></td>
+  <td class="xl74" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl76" colspan="2"></td>
+  <td class="xl76" colspan="2"></td>
+</tr>
+<tr>
+<td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+<tr>
+  <td class="xl80" colspan="2"></td>
+  <td class="xl80" colspan="2"></td>
+</tr>
+</tbody>
+</table>
+<!--Fim tabela-->
+</div>
+  
+
+  
+</body>
+</html>
 
 <?php
+/*
  include_once 'ConAL.php';
  $style = "#Modelo_body{
  	background-color: inherit;
@@ -109,7 +312,6 @@ $tabela ="<table><tbody>".$tabela."</tbody></table>";
 echo  $html;
 
 
-/*
  $arquivo = "Etiqueta.html";
  // Configurações header para forçar o download
 file_put_contents($arquivo, $html);
